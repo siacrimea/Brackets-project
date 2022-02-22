e1eb36fbf42070c7906fe3fa426e573df2377497
siacrimea <siacrimea@gmail>
2022-02-22 16:16:52 +0300

    22.02.2022

---
+++ lesson 2 ActiveBox/app.js
@@ -0,0 +1,21 @@
+$(function() {
+
+    let header = $("#header");
+    let intro = $("#intro");
+    let introH= intro.innerHeight();
+    let scrollPos = $(window).scrollTop();
+
+    
+    $(window).on("scroll load resize", function() {
+//        introH = intro.innerHeight();
+        scrollPos = $(this).scrollTop();
+//
+console.log(scrollPos);
+    //        if( scrollPos > introH ) {
+            header.addClass("fixed");
+//        } else {
+//            header.removeClass("fixed");
+//        }
+    });
+
+});
